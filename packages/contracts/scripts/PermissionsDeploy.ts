86f3b88625da559cb80d265c6a99fd93cb5b626e
kiên phạm <fbkienpt@gmail.com>
2025-09-09 22:40:30 +0700

    fix: file permission deploy

--- packages/contracts/scripts/PermissionsDeploy.ts
+++ packages/contracts/scripts/PermissionsDeploy.ts
@@ -1,35 +1,32 @@
-import { ethers, run } from "hardhat";
+import { ethers, run } from 'hardhat';
 
 async function main() {
-    await run("compile");
-    console.log("Compiled contract...");
+  await run('compile');
+  console.log('Compiled contract...');
 
-    const owner = "0xBac2B69C092d8F9D5A102D1762a197A90947DCbB";
+  console.log('Deploying Permissions...');
+  const owner = '0xEcf58FE15b7606DA86D7CAa7B58aa878D206041a';
 
-    console.log("Deploying Permissions...");
-    const Permissions = await ethers.getContractFactory("Permissions");
-    const permissions = await Permissions.deploy(); 
-    await permissions.waitForDeployment();
+  const Permissions = await ethers.getContractFactory('Permissions');
+  const permissions = await Permissions.deploy(owner);
 
-    // Lấy địa chỉ contract và đảm bảo đã resolve Promise
-    const permissionsAddr = await permissions.getAddress(); 
-    console.log("Permissions deployed to:", permissionsAddr);
+  const permissionsAddr = await permissions.getAddress();
+  console.log('Permissions deployed to:', permissionsAddr);
 
-    // Gọi initialize
-    const tx = await permissions.initialize(owner);
-    await tx.wait();
-    console.log("Permissions initialized with admin:", owner);
+  console.log('Wait to verify contract');
 
-    console.log("Wait to verify contract...");
-    await new Promise((resolve) => setTimeout(resolve, 60 * 1000));
-
-    await run("verify:verify", {
-        address: permissionsAddr, // đã là string
-        constructorArguments: [],
-    });
+  await new Promise((resolve) => {
+    setTimeout(resolve, 60 * 1000);
+  });
+  await run('verify:verify', {
+    address: permissionsAddr,
+    constructorArguments: [owner],
+  });
 }
 
-main().catch((error) => {
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
     console.error(error);
     process.exit(1);
-});
+  });
